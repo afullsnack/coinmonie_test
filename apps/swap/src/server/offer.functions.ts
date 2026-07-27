c07a4615356fa9f6469fa49090eaa4e39dc76b89
Miracle Friday <miraclef60@gmail.com>
2026-07-27 23:22:40 +0100

    refactor: Update cwd lookup

--- apps/swap/src/server/offer.functions.ts
+++ apps/swap/src/server/offer.functions.ts
@@ -88,7 +88,7 @@ export const assetList = createServerFn({ method: 'GET' }).handler(async () => {
     const dir =
       env.NODE_ENV === 'development'
         ? `./public/assets/tokens`
-        : `${process.cwd()}/assets/tokens`
+        : `/assets/tokens`
     const files = await fs.readdir(dir).catch((error: any) => {
       console.log(`Error reading files`, { error })
       return []
